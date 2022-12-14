d57b8d4d31dd960d809fab9bb7fc61fcedd31f97
Raiza de Paula <raizapaula93@gmail.com>
2022-12-13 22:37:07 -0300

    Introdução à herança

---
+++ src/TesteGerente.java
@@ -0,0 +1,17 @@
+public class TesteGerente {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("João");
+        g1.setCpf("12345678901");
+        g1.setSalario(5000);
+        g1.setSenha(2222);
+
+        System.out.println(g1.getNome());
+        System.out.println(g1.getCpf());
+        System.out.println(g1.getSalario());
+
+        boolean autenticou= g1.autentica(2222);
+
+        System.out.println(autenticou);
+    }
+}
